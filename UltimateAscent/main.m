8bf5bbe214cc40aa5fe8ad32264fc80a1f09b8f2
FRC <frc@helen.ndc.nasa.gov>
2013-01-11 18:52:31 -0600

    Initial Commit

---
+++ UltimateAscent/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UltimateAscent
+//
+//  Created by FRC on 1/11/13.
+//  Copyright (c) 2013 FRC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
