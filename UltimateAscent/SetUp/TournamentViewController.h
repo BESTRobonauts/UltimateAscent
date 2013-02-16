91efc91a6f3f2b177376cbba929630a466881a74
Kris <BESTRobonauts@gmail.com>
2013-02-15 18:08:16 -0600

    Forgot some files.

---
+++ UltimateAscent/SetUp/TournamentViewController.h
@@ -0,0 +1,13 @@
+//
+//  TournamentViewController.h
+//  UltimateAscent
+//
+//  Created by FRC on 2/15/13.
+//  Copyright (c) 2013 FRC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TournamentViewController : UIViewController
+
+@end
---
+++ UltimateAscent/SetUp/TournamentViewController.m
@@ -0,0 +1,38 @@
+//
+//  TournamentViewController.m
+//  UltimateAscent
+//
+//  Created by FRC on 2/15/13.
+//  Copyright (c) 2013 FRC. All rights reserved.
+//
+
+#import "TournamentViewController.h"
+
+@interface TournamentViewController ()
+
+@end
+
+@implementation TournamentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
